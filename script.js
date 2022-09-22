740bb0607e2ba8057c5c7d77431a3e584218d47b
Usupru <101202245+Usupru@users.noreply.github.com>
2022-09-22 12:15:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+async function typeSentence(sentence, eleRef, delay = 100) {
+  const letters = sentence.split("");
+  let i = 0;
+  while(i < letters.length) {
+    await waitForMs(delay);
+    $(eleRef).append(letters[i]);
+    i++
+  }
+  return;
+}
+
+
+function waitForMs(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+typeSentence("Hey, over here!", "a")
